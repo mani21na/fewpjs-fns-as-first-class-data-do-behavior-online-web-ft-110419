f11f0862a7a87723eddb6ed26f388f641dd9964f
Hanna Lee <mani21na@gmail.com>
2020-02-28 17:57:13 +0000

    Done.

--- index.js
+++ index.js
@@ -9,3 +9,26 @@ function handleClick(e) {
 
 /* Write your implementation of greet() */
 /* Write your implementation of displayMessage() */
+function greet(string) {
+  let message = ""
+  let splitString = string.split(':')
+  let time = splitString[0]
+  let newTime = parseInt(time, 10)
+
+  if (newTime < 12) {
+    message = "Good Morning"
+  } else if (newTime > 17) {
+    message = "Good Evening"
+  } else {
+    message = "Good Afternoon"
+  }
+
+  return message
+}
+
+/* Write your implementation of displayMessage() */
+
+function displayMessage(string) {
+  const greeting = document.querySelector('h1')
+  greeting.innerText = string
+}
